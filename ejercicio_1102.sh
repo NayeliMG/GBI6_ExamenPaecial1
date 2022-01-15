2f3c90105780965c9b3cc983536c037b6329e286
NayeliMG <94936301+NayeliMG@users.noreply.github.com>
2022-01-15 11:53:09 -0500

    Update ejercicio_1102.sh
    Instrucción del ejercicio 1.10.2

--- ejercicio_1102.sh
+++ ejercicio_1102.sh
@@ -1,1 +1,5 @@
-
+ejercicio 1.10.2 Niveles hormonales en babuinos
+Gesquiere et al (2011) estudiaron los niveles hormonales en la sangre de los babuinos.Cada individuo fue muestrado varias veces.
+1. ¿Cuántas veces se registraron los niveles de los individuos 3 y 27?
+2. Escribir un scrip tomando como entrada el nombre del archivo y el ID del individuo y devolver el número de registros para esa identificación.
+3. Escrbir un scrip que devuelva el número de veces que cada individuo fue muestrado.
